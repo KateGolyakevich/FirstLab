4db430751435ffd0bef96fcfd024022b0e91cee3
Kate Golyakevich <katdego@gmail.com>
2026-09-18 16:06:01 +0300

    aded constructor setters and getters

--- include/Service.h
+++ include/Service.h
@@ -1,13 +1,33 @@
+#ifndef SERVICE_H
+#define SERVICE_H
+
 #include <iostream>
 #include <string>
 
 class Service{
-public:
+private:
     std::string name;
     std::string doctor_name;
     int cost;
     int duration;
 
+    public:
+    Service();
+    Service(std::string name, std::string doctor_name, int cost, int duration);
+
+    ~Service();
+
+    std::string get_serve_name()const;
+    std::string get_doctor_name()const;
+    int get_cost() const;
+    int get_duration() const;
+
+    void set_name(const std::string& name);
+    void set_doctor_name(const std::string& doctor_name);
+    void set_cost(int cost);
+    void set_duration(int dur);
+
     void print();
 };
 
+#endif
